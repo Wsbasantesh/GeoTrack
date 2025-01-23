662bcd467e584b76675dd1691d9b4d954d774427
Wilians Basantes <wsbasantesh@uce.edu.ec>
2025-01-23 18:48:34 -0500

    Create TblClientEndpoints.cs

---
+++ Endpoints/TblClientEndpoints.cs
@@ -0,0 +1,73 @@
+﻿
+using GeoTrack_Services.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GeoTrack_Services.Endpoints;
+
+public static class TblClientEndpoints
+{
+    public static void MapTblClientEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/TblClient").WithTags(nameof(TblClient));
+
+        group.MapGet("/", async (GeoTrackContext db) =>
+        {
+            return await db.TblClient.ToListAsync();
+        })
+        .WithName("GetAllTblClients")
+        .WithOpenApi();
+
+      */
+        group.MapGet("/{id}", async Task<Results<Ok<TblLogin>, NotFound>> (int id, GeoTrackContext db) =>
+        {
+            return await db.TblLogins.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.Id == id)
+                is TblLogin model
+                    ? TypedResults.Ok(model)
+                    : TypedResults.NotFound();
+        })
+        .WithName("GetTblLoginById")
+        .WithOpenApi();
+
+        group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int id, TblLogin tblLogin, GeoTrackContext db) =>
+        {
+            var affected = await db.TblLogins
+                .Where(model => model.Id == id)
+                .ExecuteUpdateAsync(setters => setters
+                  .SetProperty(m => m.Id, tblLogin.Id)
+                  .SetProperty(m => m.User, tblLogin.User)
+                  .SetProperty(m => m.Password, tblLogin.Password)
+                  .SetProperty(m => m.Imei, tblLogin.Imei)
+                  .SetProperty(m => m.Token, tblLogin.Token)
+                  .SetProperty(m => m.DateCreation, tblLogin.DateCreation)
+                  .SetProperty(m => m.ExpiredDate, tblLogin.ExpiredDate)
+                  .SetProperty(m => m.Rol, tblLogin.Rol)
+                  );
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("UpdateTblLogin")
+        .WithOpenApi();
+
+        group.MapPost("/", async (TblLogin tblLogin, GeoTrackContext db) =>
+        {
+            db.TblLogins.Add(tblLogin);
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/api/TblLogin/{tblLogin.Id}", tblLogin);
+        })
+        .WithName("CreateTblLogin")
+        .WithOpenApi();
+
+        group.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (int id, GeoTrackContext db) =>
+        {
+            var affected = await db.TblLogins
+                .Where(model => model.Id == id)
+                .ExecuteDeleteAsync();
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("DeleteTblLogin")
+        .WithOpenApi();
+      */
+    }
+}
