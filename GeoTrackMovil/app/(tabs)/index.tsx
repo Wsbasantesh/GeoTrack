2d978f271ff990d1170885e6d6eb8b28272226b4
MauriZio03 <mauri.megax@gmail.com>
2025-01-30 01:53:14 -0500

    add_indexlogin

--- GeoTrackMovil/app/(tabs)/index.tsx
+++ GeoTrackMovil/app/(tabs)/index.tsx
@@ -1,37 +1,54 @@
-import React, { useState } from 'react';
-import { Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-/*
-We'll use Expo Router's Link component to navigate from the /index route to the /about route. It is a React component that renders a <Text> with a given href prop.
-*/
-import { Link } from 'expo-router';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { firebaseConfig } from '../../firebase-config';
+import { initializeApp } from 'firebase/app';
+import Logo from '../../components/logo'; 
 
-
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
-    </View>
-  );
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      if (user) {
+        console.log("Usuario logueado");
+        router.push("/about");
+      } else {
+        console.log("Usuario no logueado");
+        router.push("/login");
+      }
+    });
+
+    return () => unsubscribe();
+  }, [router]);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <Logo style={styles.image} />
+      </View>
+    );
+  }
+
+  return null;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: '#1287C3', 
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  image: {
+    width: 200, 
+    height: 200,
   },
 });
-
+//
