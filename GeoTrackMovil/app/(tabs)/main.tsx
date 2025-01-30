34a6756e052f983f2ff8793f00f075be95c353a5
Wilians Basantes <wsbasantesh@uce.edu.ec>
2025-01-30 11:02:52 -0500

    Update main.tsx

--- GeoTrackMovil/app/(tabs)/main.tsx
+++ GeoTrackMovil/app/(tabs)/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, StatusBar } from 'react-native';
-import Logo from '../components/Logo';
-import Button from '../components/Button';
+import Logo from '../../components/Logo';
+import Button from '../../components/Button';
 
 export default function MainScreen() {
   return (
